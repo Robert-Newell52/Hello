5293742dd6068da443a5e00f4b29ab60800f58da
Robert-Newell52 <rnewell10@huskers.unl.edu>
2020-08-24 20:46:36 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -4,26 +4,17 @@
  * Date: 2020/08/24
  *
  * A C program that states my name.
- * 
+ *
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
 
-  printf("Robert Newell!\n");
-  printf("Robert Newell!\n");
-  printf("Robert Newell!\n");
-  printf("Robert Newell!\n");
-  printf("Robert Newell!\n");
-  printf("Robert Newell!\n");
-  printf("Robert Newell!\n");
-  printf("Robert Newell!\n");
-  printf("Robert Newell!\n");
-  printf("Robert Newell!\n");
-  printf("Robert Newell!\n");
-  printf("Robert Newell!\n");
-  
+  printf("Robert Newell 1!\n");
+  printf("Computer Science!\n");
+
+
 
   return 0;
 }
