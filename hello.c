fd911ef644d9d3d4edc51c52dea8601062682683
Robert-Newell52 <rnewell10@huskers.unl.edu>
2020-08-24 20:33:39 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,29 @@
+/**
+ * Author: Robert Newell
+ *
+ * Date: 2020/08/24
+ *
+ * A C program that states my name.
+ * 
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Robert Newell!\n");
+  printf("Robert Newell!\n");
+  printf("Robert Newell!\n");
+  printf("Robert Newell!\n");
+  printf("Robert Newell!\n");
+  printf("Robert Newell!\n");
+  printf("Robert Newell!\n");
+  printf("Robert Newell!\n");
+  printf("Robert Newell!\n");
+  printf("Robert Newell!\n");
+  printf("Robert Newell!\n");
+  printf("Robert Newell!\n");
+  
+
+  return 0;
+}
